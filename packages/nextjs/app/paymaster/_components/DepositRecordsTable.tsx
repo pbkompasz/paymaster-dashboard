b74d424e7cfbb06c55ae6060bce004292487585f
nick-dev123 <144809451+nick-dev123@users.noreply.github.com>
2025-01-28 21:16:31 +0200

    chore: table

--- packages/nextjs/app/paymaster/_components/DepositRecordsTable.tsx
+++ packages/nextjs/app/paymaster/_components/DepositRecordsTable.tsx
@@ -15,20 +15,46 @@ export const DepositRecordsTable = () => {
 
   return (
     <div className="flex text-sm rounded-3xl peer-checked:rounded-b-none min-h-0 bg-secondary py-0">
-      <table>
-        <th>
-          <td>Date & Time</td>
-          <td>Network</td>
-          <td>Amount</td>
-          <td>From</td>
-          <td>Tx</td>
-          <td>Balance</td>
-        </th>
-        {depositRecords.map(deposit => (
-          <tr key={deposit.tx}>
-            <td></td>
+      <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <thead className="text-xs text-gray-700 uppercase dark:text-gray-400">
+          <tr>
+            <th scope="col" className="px-6 py-3 bg-gray-50 dark:bg-gray-800">
+              Date & Time
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Network
+            </th>
+            <th scope="col" className="px-6 py-3 bg-gray-50 dark:bg-gray-800">
+              Amount
+            </th>
+            <th scope="col" className="px-6 py-3">
+              From
+            </th>
+            <th scope="col" className="px-6 py-3 bg-gray-50 dark:bg-gray-800">
+              Tx
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Balance
+            </th>
           </tr>
-        ))}
+        </thead>
+        <tbody>
+          {depositRecords.map(deposit => (
+            <tr key={deposit.tx} className="border-b border-gray-200 dark:border-gray-700">
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800"
+              >
+                {+deposit.date}
+              </th>
+              <td className="px-6 py-4">{deposit.network}</td>
+              <td className="px-6 py-4 bg-gray-50 dark:bg-gray-800">{deposit.amount}</td>
+              <td className="px-6 py-4">{deposit.fromAddress}</td>
+              <td className="px-6 py-4">{deposit.tx}</td>
+              <td className="px-6 py-4">{deposit.balanceAfter}</td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </div>
   );
